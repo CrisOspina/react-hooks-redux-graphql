58b100375a039dc0ba8903f9032fd9980d6634f2
CrisOspina <crisospina523@gmail.com>
2020-01-06 16:56:59 -0500

    [add] page-favs

---
+++ src/pages/favs/FavPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './favs.module.css';
+import Card from '../../components/card/Card';
+
+export default function FavPage({ characters = [0] }) {
+  function renderCharacter(char, i) {
+    return <Card key={i} />;
+  }
+  return (
+    <div className={styles.container}>
+      <h2>Favoritos</h2>
+      {characters.map(renderCharacter)}
+      {!characters.length && <h3>No hay personajes agregados</h3>}
+    </div>
+  );
+}
