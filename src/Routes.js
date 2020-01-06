23d87e6fe45a4c7ffa8f8ca9b8756fa65d2b8e58
CrisOspina <crisospina523@gmail.com>
2020-01-06 16:55:30 -0500

    [update] routes-pages-app

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from './components/home/HomePage'
-import FavPage from './components/favs/FavPage'
-import LoginPage from './components/login/LoginPage'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/home/HomePage';
+import FavPage from './pages/favs/FavPage';
+import LoginPage from './pages/login/LoginPage';
 
 export default function Routes() {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/favs" component={FavPage} />
-            <Route path="/login" component={LoginPage} />
-        </Switch>
-    )
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/favs' component={FavPage} />
+      <Route path='/login' component={LoginPage} />
+    </Switch>
+  );
 }
