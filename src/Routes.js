7b3ccbb07ac87544c900ae2017695c41188b2c6a
crisOspina523@gmail.com <crisospina523@gmail.com>
2020-04-11 21:36:40 -0500

    [add] import private routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './pages/home/HomePage';
-import FavPage from './pages/favs/FavPage';
-import LoginPage from './pages/login/LoginPage';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/home/HomePage'
+import FavPage from './pages/favs/FavPage'
+import LoginPage from './pages/login/LoginPage'
+import PrivateRoutes from './PrivateRoutes'
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/favs' component={FavPage} />
+      <PrivateRoutes exact path='/' component={Home} />
+      <PrivateRoutes path='/favs' component={FavPage} />
       <Route path='/login' component={LoginPage} />
     </Switch>
-  );
+  )
 }
+
+
+
+
+
+
+// Iniciar sesión dependiendo del localstarage
+// const PrivateRoute = ({
+//   component,
+//   path, 
+//   ...props
+// }) => {
+//   let storage = localStorage.getItem('storage')
+//   storage = JSON.parse(storage)
+
+//   if(storage && storage.loggIn.loggedIn) {
+//     return (
+//       <Route 
+//         path={path}
+//         component={component}
+//         {...props}>
+//       </Route>
+//     )
+//   } else {
+//     return (
+//       <Redirect to='/login/' {...props} />
+//     )
+//   }
+// }
+
+// export default function Routes() {
+//   return (
+//     <Switch>
+//       <PrivateRoute exact path='/' component={Home} />
+//       <PrivateRoute path='/favs' component={FavPage} />
+//       <Route path='/login' component={LoginPage} />
+//     </Switch>
+//   )
+// }
