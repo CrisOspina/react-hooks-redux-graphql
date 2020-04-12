d3df5fb1150279c43007a26443dcfdbed68fb8e0
crisOspina523@gmail.com <crisospina523@gmail.com>
2020-04-11 21:35:15 -0500

    [add] semicolon false

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import Routes from './Routes';
-import { Navbar } from './components/navbar/Navbar';
+import React from 'react'
+import './App.css'
+import Routes from './Routes'
+import { Navbar } from './components/navbar/Navbar'
 
 export default function App() {
   return (
